88560b222ddea298944dafae8073720d4a189990
Adam Bysice <adambysice@gmail.com>
2016-01-15 16:16:59 -0500

    skeleton

---
+++ RoboGundamSpaceShip/Assets/Development/Scripts/Centralized/Enums.cs
@@ -0,0 +1,19 @@
+﻿//All enums centralized here
+//
+//
+
+using UnityEngine;
+using System.Collections;
+
+public class Enums : MonoBehaviour {
+
+    public enum GameStateNames
+    {
+        GS_00_NULL = -1,
+        GS_01_MENU = 0,
+        GS_02_LOADING = 1, 
+        GS_03_INPLAY,
+        GS_04_LEAVING
+    };
+
+}
---
+++ RoboGundamSpaceShip/Assets/Development/Scripts/Managers/GameStateManager/States/GameStateMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateMenu : GameStateBase
+{
+	public GameStateMenu(GameStateManager p_gameStateManager)
+	{
+		m_gameStateManager = p_gameStateManager;
+	}
+
+
+	public override void EnterState(Enums.GameStateNames p_prevState)
+	{
+		Debug.Log("Entered Menu State");
+	}
+
+	public override void UpdateState()
+	{
+		//Put some sort of GUI event to get to this
+		m_gameStateManager.ChangeGameState(Enums.GameStateNames.GS_02_LOADING);
+	}
+
+	public override void ExitState(Enums.GameStateNames p_nextState)
+	{
+
+	}
+}
