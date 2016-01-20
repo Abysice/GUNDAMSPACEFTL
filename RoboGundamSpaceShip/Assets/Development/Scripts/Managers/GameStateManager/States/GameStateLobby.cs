f02e48e6d0b2defbda3db43ad561a0a7fbf916ab
Adam Bysice <adambysice@gmail.com>
2016-01-20 06:14:11 -0500

    new files for testing stuff

---
+++ RoboGundamSpaceShip/Assets/Development/Scripts/Managers/GameStateManager/States/GameStateLobby.cs
@@ -0,0 +1,39 @@
+﻿// Script Comments go here
+//
+//
+
+using UnityEngine;
+using System.Collections;
+
+public class GameStateLobby : GameStateBase
+{
+	private GameObject m_lobbyCamera = null;
+
+	public GameStateLobby(GameStateManager p_gameStateManager)
+	{
+		m_gameStateManager = p_gameStateManager;
+	}
+
+
+	public override void EnterState(Enums.GameStateNames p_prevState)
+	{
+		Debug.Log("Entered Lobby state");
+		//lobby placeholder
+		m_lobbyCamera = GameObject.Instantiate(Managers.GetInstance().GetGameProperties().MenuCamera);
+		Managers.GetInstance().GetGUIManager().LoadLobbyGUI();//spawn menu gui
+	}
+
+	public override void UpdateState()
+	{
+		if(!m_lobbyCamera)
+		{
+
+		}
+
+	}
+
+	public override void ExitState(Enums.GameStateNames p_nextState)
+	{
+		GameObject.Destroy(m_lobbyCamera);
+	}
+}
