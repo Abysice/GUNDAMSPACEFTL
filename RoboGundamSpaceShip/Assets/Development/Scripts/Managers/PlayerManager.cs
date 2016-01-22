dab8c10f06d87dfc76101b1503d7c9392881601b
Adam Bysice <adambysice@gmail.com>
2016-01-22 16:50:36 -0500

    added some new files (may remove later)

---
+++ RoboGundamSpaceShip/Assets/Development/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,50 @@
+﻿// This class will keep track of the player prefabs and such and such
+//
+// Written by: Adam Bysice
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerManager : NetworkBehaviour {
+
+	#region Public Variables
+	#endregion
+
+	#region Protected Variables
+	#endregion
+
+	#region Private Variables
+	#endregion
+
+	#region Accessors
+	#endregion
+
+	#region Unity Defaults
+	//initialization
+	public void Start()
+	{
+
+	}
+	//runs every frame
+	public void Update()
+	{
+
+	}
+	#endregion
+
+	#region Public Methods
+	[Command]
+	public void CmdSpawnPlayer()
+	{
+		GameObject l_player = GameObject.Instantiate(Managers.GetInstance().GetGameProperties().playerPrefab);
+		ClientScene.RegisterPrefab(Managers.GetInstance().GetGameProperties().playerPrefab);
+		NetworkServer.Spawn(l_player);
+	}
+	#endregion
+
+	#region Protected Methods
+	#endregion
+
+	#region Private Methods
+	#endregion
+}
---
+++ RoboGundamSpaceShip/Assets/Development/Scripts/PawnMaster/PawnController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+
+public class PawnController : NetworkBehaviour {
+	#region Public Variables
+	public float PLAYER_MOVE_MULTIPLIER = 0.05f; //make a const later
+	#endregion
+
+	#region Protected Variables
+	#endregion
+
+	#region Private Variables
+	[SyncVar] private Vector2 m_playerPosition; //synchronized over the network
+	private GameObject m_localGameCamera;
+	#endregion
+
+	#region Accessors
+	#endregion
+
+	#region Unity Defaults
+	//initialization
+	public void Start()
+	{
+		m_playerPosition = transform.position;
+		//spawn local Camera
+		GameObject m_localGameCamera = GameObject.Instantiate(Managers.GetInstance().GetGameProperties().playerCamera);
+	}
+	//runs every frame
+	public void Update()
+	{
+		if (isLocalPlayer) //is this my player? return if its not mine
+		{
+			Debug.Log("RETURNING LOL");
+			return;
+		}
+			
+		
+		//placeholder movement
+		if (Input.GetKey(KeyCode.UpArrow))
+			m_playerPosition += (new Vector2(0, 1) * PLAYER_MOVE_MULTIPLIER);
+		else if (Input.GetKey(KeyCode.DownArrow))
+			m_playerPosition += (new Vector2(0, -1) * PLAYER_MOVE_MULTIPLIER);
+		if (Input.GetKey(KeyCode.RightArrow))
+			m_playerPosition += (new Vector2(1, 0) * PLAYER_MOVE_MULTIPLIER);
+		else if (Input.GetKey(KeyCode.LeftArrow))
+			m_playerPosition += (new Vector2(-1, 0) * PLAYER_MOVE_MULTIPLIER);
+
+		transform.position = m_playerPosition;
+	}
+	#endregion
+
+	#region Public Methods
+		#endregion
+
+	#region Protected Methods
+	#endregion
+
+	#region Private Methods
+	#endregion
+}
