112cd44987254e3328f838cd506c7dd7438dd960
Adam Bysice <adambysice@gmail.com>
2016-01-30 08:58:50 -0500

    new ship!

---
+++ RoboGundamSpaceShip/Assets/Development/Scripts/PawnMaster/ShipController.cs
@@ -0,0 +1,43 @@
+﻿// Controller class for the ship, will be complicated
+//
+// Written by: Adam Bysice
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ShipController : NetworkBehaviour {
+
+	#region Public Variables
+	#endregion
+
+	#region Protected Variables
+	#endregion
+
+	#region Private Variables
+	#endregion
+
+	#region Accessors
+	#endregion
+
+	#region Unity Defaults
+	//initialization
+	public void Start()
+	{
+
+	}
+	//runs every frame
+	public void Update()
+	{
+		// will need to be given AssignClientAuthority by the server before you can control
+	}
+	#endregion
+
+	#region Public Methods
+	#endregion
+
+	#region Protected Methods
+	#endregion
+
+	#region Private Methods
+	#endregion
+}
