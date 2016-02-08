fcbd3729d12b5ea15dd81cbd59f6ba819284dbc9
Bryan <ramoulb@gmail.com>
2016-02-08 16:37:42 -0500

    new killable and damageable interface

---
+++ RoboGundamSpaceShip/Assets/Development/Scripts/PawnMaster/PawnProperties/IKillable.cs
@@ -0,0 +1,12 @@
+﻿// Interface class for all objects that are "Killable"
+//
+// Written by: Bryan Ramoul
+using UnityEngine;
+using System.Collections;
+
+public interface IKillable {
+
+    //to be called when the killable object's HP is less or equal to zero
+    void Kill();
+
+}
