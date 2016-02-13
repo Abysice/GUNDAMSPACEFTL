57b377f79e6c0b3b12411a66df20dac2ab3554ba
Adam Bysice <adambysice@gmail.com>
2016-02-12 19:35:36 -0500

    Turret rotational restrictions implemented

--- RoboGundamSpaceShip/Assets/Development/Scripts/Managers/PlayerManager.cs
+++ RoboGundamSpaceShip/Assets/Development/Scripts/Managers/PlayerManager.cs
@@ -67,24 +67,24 @@ public class PlayerManager : NetworkBehaviour {
 		for (int i = 0; i < NetworkServer.connections.Count; i++)
 		{
 			Debug.Log("spawnin a dude for :" + NetworkServer.connections[i]);
-			GameObject l_player = GameObject.Instantiate(Managers.GetInstance().GetGameProperties().playerPrefab);
-			l_player.transform.position = m_spawns[i].position;
+			GameObject l_player =(GameObject)Instantiate(Managers.GetInstance().GetGameProperties().playerPrefab, m_spawns[i].position, Quaternion.identity);
+			//l_player.transform.position = m_spawns[i].position;
 			
 			NetworkServer.AddPlayerForConnection(NetworkServer.connections[i], l_player, 0);
 			//l_player.transform.parent = m_ship.transform;
 		}
 		
 		//Spawn Mecha
-		m_gundam = GameObject.Instantiate(Managers.GetInstance().GetGameProperties().mechaPrefab);
+		m_gundam = (GameObject)Instantiate(Managers.GetInstance().GetGameProperties().mechaPrefab, m_spawns[3].position, Quaternion.identity);
 		NetworkServer.Spawn(m_gundam);
-		m_gundam.transform.position = m_spawns[3].position;
-		m_gundam.GetComponent<MechaController>().RpcSetPosition(m_gundam.transform.position);
-
+		//m_gundam.transform.position = m_spawns[3].position;
+		//m_gundam.GetComponent<MechaController>().RpcSetPosition(m_gundam.transform.position);
+ 
 		//spawn the turrets
 		for (int i = 0; i < 4; i++)
 		{
-			m_cannons[i] = GameObject.Instantiate(Managers.GetInstance().GetGameProperties().cannonPrefab);
-			m_cannons[i].transform.position = m_spawns[i+4].position;
+			m_cannons[i] = (GameObject)Instantiate(Managers.GetInstance().GetGameProperties().cannonPrefab, m_spawns[i + 4].position, Quaternion.identity);
+			//m_cannons[i].transform.position = m_spawns[i+4].position;
 			NetworkServer.Spawn(m_cannons[i]);
 			m_cannons[i].GetComponent<TurretController>().RpcSetPosition(m_cannons[i].transform.position);
 		}
--- RoboGundamSpaceShip/Assets/Development/Scripts/PawnMaster/Controllers/TurretController.cs
+++ RoboGundamSpaceShip/Assets/Development/Scripts/PawnMaster/Controllers/TurretController.cs
@@ -8,6 +8,8 @@ using System.Collections;
 public class TurretController : NetworkBehaviour, IEnterable {
 
 	#region Public Variables
+	public float TRACKING_SPEED = 5.0f;
+	public float m_angle;
 	#endregion
 
 	#region Protected Variables
@@ -17,6 +19,8 @@ public class TurretController : NetworkBehaviour, IEnterable {
 	private SpriteRenderer m_renderer;
 	private CameraController m_camCont;
 	private GameObject m_PlayerCamera;
+	private GameObject m_ship;
+	private bool m_rightSide = false;
 	#endregion
 
 	#region Accessors
@@ -29,6 +33,15 @@ public class TurretController : NetworkBehaviour, IEnterable {
 		m_renderer = gameObject.GetComponent<SpriteRenderer>();
 		m_PlayerCamera = Managers.GetInstance().GetGameStateManager().GetPlayerCamera();
 		m_camCont = m_PlayerCamera.GetComponent<CameraController>();
+		m_ship = Managers.GetInstance().GetPlayerManager().m_ship;
+		
+		Vector2 l_shipPos = m_ship.transform.position;
+		Vector2 l_turretPos = gameObject.transform.position;
+		//figure out which side your on
+		if (l_shipPos.x < l_turretPos.x)
+			m_rightSide = true;
+		else
+			m_rightSide = false;
 	}
 	//runs every frame
 	public void Update()
@@ -41,10 +54,14 @@ public class TurretController : NetworkBehaviour, IEnterable {
 		Vector3 l_mpos = Input.mousePosition;
 		l_mpos = Camera.main.ScreenToWorldPoint(l_mpos);
 		l_mpos = l_mpos - transform.position;
-		float angle = Mathf.Atan2(l_mpos.y, l_mpos.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.AngleAxis(angle-90.0f, Vector3.forward);
-		
+	
+		//returns -1 when to the left, 1 to the right
+		int l_ret = AngleDir(l_mpos, m_ship.transform.up, m_PlayerCamera.transform.forward);
+		if ((m_rightSide && l_ret == 1) || (!m_rightSide && l_ret == -1))
+			m_angle = Mathf.Atan2(l_mpos.y, l_mpos.x) * Mathf.Rad2Deg;
 
+		Quaternion l_rot = Quaternion.AngleAxis(m_angle - 90.0f, Vector3.forward);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, l_rot, TRACKING_SPEED);
 	}
 	#endregion
 
@@ -73,5 +90,19 @@ public class TurretController : NetworkBehaviour, IEnterable {
 	#endregion
 
 	#region Private Methods
+	//returns -1 when to the left, 1 to the right, and 0 for forward/backward
+	public int AngleDir(Vector3 p_fwd, Vector3 p_targetDir, Vector3 p_up)
+	{
+		Vector3 l_perp = Vector3.Cross(p_fwd, p_targetDir);
+		float l_dir = Vector3.Dot(l_perp, p_up);
+
+		if (l_dir > 0.0f)
+			return 1;
+		else if (l_dir < 0.0f)
+			return -1;
+		else
+			return 0;
+	}  
+ 
 	#endregion
 }
